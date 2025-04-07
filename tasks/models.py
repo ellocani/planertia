980bfc577a659cd2e1cb875230be41204f9d6bd6
min130111 <min130111@naver.com>
2025-04-08 07:32:12 +0900

    Task 모델 생성 및 마이그레이션

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
