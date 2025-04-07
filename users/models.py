de487dab2e386092e8b775d8073040717bad6d55
min130111 <min130111@naver.com>
2025-04-08 07:17:10 +0900

    회원가입 구현

--- users/models.py
+++ users/models.py
@@ -1,4 +1,48 @@
-
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError("이메일은 필수입니다.")
+        if not username:
+            raise ValueError("사용자명은 필수입니다.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("슈퍼유저는 is_staff=True 여야 합니다.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("슈퍼유저는 is_superuser=True 여야 합니다.")
+        
+        return self.create_user(email, username, password, **extra_fields)
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSignupSerializer
 
-# Create your views here.
+
+class UserSignupView(APIView):
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"회원가입이 완료되었습니다."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
