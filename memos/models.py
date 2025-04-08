0694d4c89c3a6c5a4c2f45fc3a07c8b1bdcbe4bd
min130111 <min130111@naver.com>
2025-04-08 21:01:45 +0900

    memo, task 연결 및 오류 수정

--- memos/models.py
+++ memos/models.py
@@ -4,11 +4,19 @@ from tasks.models import Task
 
 
 class Memo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='memos',)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='momos',)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="memos",
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="memos",
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"Memo by {self.user.username} on {self.task.title}"
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Task
+from memos.serializers import MemoSerializer
 
 
 class TaskSerializer(serializers.ModelSerializer):
-
+    memos = MemoSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Task
         fields = "__all__"
