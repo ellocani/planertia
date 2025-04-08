8c2e49353d6facc8766031c552b1f67fc0d0d449
min130111 <min130111@naver.com>
2025-04-08 10:25:28 +0900

    admin 패널 등록

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'is_completed', 'due_date')
+    list_filter = ('is_completed', 'due_date')
+    search_fields = ('title', 'description')
+
+
+admin.site.register(Task, TaskAdmin)
