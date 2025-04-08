0d058e210d43b162b34dee8abb4c1519141cef78
min130111 <min130111@naver.com>
2025-04-08 09:55:35 +0900

    Category 모델 생성 및 마이그레이션

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="categories",
+    )
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    color = models.CharField(max_length=7, default="#FFFFFF")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
